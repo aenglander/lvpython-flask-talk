3367e0e88ce9f4171060fe905e0bc483f0586a71
Adam <adam@launchkey.com>
2015-06-07 14:20:38 -0700

    Add Jinja 2 template and stack the routes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', say='Hello World!')
 
+@app.route('/')
 @app.route('/hello/<noun>')
-def hello_noun(noun):
-    return 'Hello %s!' % noun
+def hello_noun(noun='World'):
+    return render_template('index.html', say=noun)
 
 if __name__ == '__main__':
     app.run(debug=True)
