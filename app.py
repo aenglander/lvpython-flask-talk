6575d37ff762c7405aa1522da7610a34c71bf76c
Adam <adam@launchkey.com>
2015-06-07 14:16:28 -0700

    Add variable routes

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/hello/<noun>')
+def hello_noun(noun):
+    return 'Hello %s!' % noun
+
 if __name__ == '__main__':
     app.run(debug=True)
